3f8baf6183ccebf03700f1a9263261f6ee8b575e
bruce wayne <no1_ldc@hotmail.com>
2015-03-11 11:16:48 -0400

    add helper_controller and modified layout.rb

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,3 +1,37 @@
 get '/' do
-  erb :index
+   erb :index
+end
+
+get '/login' do
+  erb :login
+end
+
+get '/signup' do
+  erb :signup
+end
+
+get '/logout' do
+  session.delete(:user_id)
+  redirect '/'
+end
+
+post '/login' do
+  user = User.authenticate(params[:name], params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect '/posts'
+  else
+    redirect '/'
+  end
+end
+
+
+post '/signup' do
+  if params[:password] == params[:password_confirmation]
+    new_user = User.create(name: params[:name], password: params[:password], password_confirmation: params[:password_confirmation])
+    session[:user_id] = new_user.id
+    redirect '/index'
+  else
+    redirect '/'
+  end
 end
