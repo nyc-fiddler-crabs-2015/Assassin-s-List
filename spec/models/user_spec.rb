0689b99cd52fc65e75c73f1f62d196f466fe878d
bruce wayne <no1_ldc@hotmail.com>
2015-03-11 20:13:37 -0400

    add another test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,23 @@
-require_relative '../spec_helper'
+# require_relative '../spec_helper'
 
-describe 'Model' do
+# describe 'Model' do
 
-  describe "create user" do
-  let(:user) {User.create(name: 'danny', password: '1234' ) }
-  it 'create a new user' do
-    expect(User.find_by(name: 'danny').name).to eq('danny')
+#   describe "create user" do
+#   let(:user) {User.create(name: 'danny', password: '1234' ) }
+#   it 'create a new user' do
+#     expect(User.find_by(name: 'danny').name).to eq('danny')
+#   end
+#  end
+# end
+
+
+require_relative "../spec_helper"
+
+describe "User Model" do
+  let(:dongchul){User.create(name:"dongchul", password:"0000")}
+
+  it 'should have a name' do
+    expect(dongchul.name).to eq("dongchul")
   end
- end
+
 end
